a45ae3e8820448a6f4bd10b8467f6c0e3008bbd2
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-03-19 19:28:20 -0500

    toastify add

--- Assigment-two/src/Components/CardOne/CardOne.jsx
+++ Assigment-two/src/Components/CardOne/CardOne.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from'../Data/cardoneData.json'
 import "./cardOne.css"
+import {toast} from "react-toastify"
 const CardOne = () => {
+
+  const[add , setAdd]=useState(false)
+  const clickHandler=()=>{
+    setAdd(!add);
+    add?(toast.error("bhai hat gaya ")):(toast.success("bhai ho gaya add"))
+  }
   return (
     <div className='food-tem'>
       {
@@ -15,7 +22,7 @@ const CardOne = () => {
                   </div>
                   <div className="image">
                     <img src={index.image} alt="" />
-                  <button>{index.button}</button>
+                  <button onClick={clickHandler}>{`${add?("Added"):("Add")}`}</button>
                   </div>
                   </div>
                   
